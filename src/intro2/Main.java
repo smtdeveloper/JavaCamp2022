9fd83958c4cec4698282f0488988c3936397388c
Samet Akca <developer2@cevizsoft.com>
2022-09-26 15:35:06 +0300

    UPDATE

---
+++ src/intro2/Main.java
@@ -0,0 +1,66 @@
+package intro2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+        String ortaMetin = "Ýlginizi Çekebilir";
+        String altMetin = "Vade Süresi";
+        System.out.println(ortaMetin);
+        // integer veri tipi sayýsal deðerler tutar
+        int vade = 12;
+
+        // double veri tipi ondalýklý sayýsal deðerler tutar
+        double dolarDun = 18.20;
+        double dolarBugun = 18.30;
+
+        // boolean veri tipi true veya false deðerler tutar
+        boolean dolarDustuMu = false;
+        String okYonu = "";
+        if (dolarBugun < dolarDun) {
+            okYonu = "down.svg";
+            System.out.println(okYonu);
+        }else if(dolarBugun > dolarDun){
+            okYonu = "up.svg";
+            System.out.println(okYonu);
+        } else {
+            okYonu = "equal.svg";
+            System.out.println(okYonu);
+        }
+
+        // Array veri tipi birden fazla veriyi tutar
+        String[] krediler = {"Hýzlý Kredi","Maaþýný Halkban'tan Alanlar","Mutlu Emekli"};
+        System.out.println(krediler[0]); // Arraylerde ilk eleman 0'dan baþlar. Kodlamada saymaya 0'dan baþlar.
+        System.out.println(krediler[1]);
+        System.out.println(krediler[2]);
+        System.out.println("-----------------------------------");
+        // for döngüsü
+        for (int i = 0; i < krediler.length; i++) {
+            System.out.println(krediler[i]);
+        }
+
+        System.out.println("-----------------------------------");
+    }
+
+}
+
+/*
+* Java da Main kodu çalýþtýrmak için gerekli metottur.
+* Java da her satýrýn sonuna noktalý virgül (;) konulur.
+* System.out.println(); kodu ekrana yazdýrma iþlemi yapar.
+* // Tekli yorum satýrý
+* Deðiþken tanýmlamak için deðiþkenin tipi ve deðiþkenin adý yazýlýr.
+Deðiþkenin tipi String, int, double, float, boolean, char, long, short, byte olabilir.
+DeðiþkenTipi deðiþkenAdý = deðer;
+* if (koþul) {
+    // Koþul saðlandýðýnda çalýþacak kodlar
+}else if(koþul2){
+    // Koþul2 saðlandýðýnda çalýþacak kodlar
+} else {
+    // Koþul saðlanmadýðýnda çalýþacak kodlar
+}
+* For Döngüsü
+* for(baþlangýç; koþul; artýþ) {
+    // Koþul saðlandýðý sürece çalýþacak kodlar
+}
+*  */
